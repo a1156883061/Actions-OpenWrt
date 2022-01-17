e7b70912fbcff036584f011431580460eba693f1
a1156883061 <36263893+a1156883061@users.noreply.github.com>
2022-01-17 16:44:43 +0800

    设置默认IP为 192.168.199.1

--- diy-part1.sh
+++ diy-part1.sh
@@ -16,3 +16,5 @@
 # Add a feed source
 #echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
+# 设置默认IP为 192.168.7.1
+sed -i 's/192.168.1.1/192.168.7.1/g' package/base-files/files/bin/config_generate
